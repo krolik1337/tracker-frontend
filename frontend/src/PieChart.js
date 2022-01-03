644ebe38ab08c3a35e55591915e6eda40085f2b8
Kamil Król <kamil.krol@justpremium.com>
2022-01-03 19:46:30 +0100

    Cleanup

--- frontend/src/PieChart.js
+++ frontend/src/PieChart.js
@@ -3,7 +3,7 @@ const { reactiveProp } = mixins
 export default {
   extends: Doughnut,
   mixins: [reactiveProp],
-  props: [ "options"],
+  props: ["options"],
   mounted() {
     // this.chartData is created in the mixin.
     // If you want to pass options please create a local options object
